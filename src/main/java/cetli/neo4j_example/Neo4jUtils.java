049c361b0fc06622b4b0de0ec6c01f6ee43252a1
Lehel44 <Lehel44@hotmail.com>
2017-01-24 08:50:51 -0800

    Recommendation fix.

--- src/main/java/cetli/neo4j_example/Neo4jUtils.java
+++ src/main/java/cetli/neo4j_example/Neo4jUtils.java
@@ -2,6 +2,7 @@ package cetli.neo4j_example;
 
 public interface Neo4jUtils {
 
-	static String databasePath = "C:\\Users\\Lehel\\Documents\\Neo4j\\default.graphdb";
+	static String databasePath = "/home/meres/Downloads/neo4j-community-3.1.0/data/databases/graph.db";
+	static String resultDataPath = "result.txt";
 
 }
--- src/main/java/cetli/neo4j_example/RecommendationEngine.java
+++ src/main/java/cetli/neo4j_example/RecommendationEngine.java
@@ -3,19 +3,22 @@ package cetli.neo4j_example;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.neo4j.graphdb.Result;
+
+import com.google.common.collect.ImmutableMap;
 
 public class RecommendationEngine {
 
 	private Graph graph;
 	private Writer writer;
-	
+
 	public RecommendationEngine(Graph graph) {
 		this.graph = graph;
 		try {
-			writer = new FileWriter("result.txt");
+			writer = new FileWriter(Neo4jUtils.resultDataPath);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -23,34 +26,48 @@ public class RecommendationEngine {
 
 	public String queryRelatedProducts(String customerId) {
 		StringBuilder resultBuilder = new StringBuilder();
-		Result result = graph.getGraphDb().execute(
-				"MATCH (customer1:CUSTOMER {customerId: \"" + customerId + "\"})-[:OWN]->(list1:SHOPPING_LIST)-[:CONTAIN]->(products:PRODUCT)"
-				+ " <-[:CONTAIN]-(list2:SHOPPING_LIST)<-[:OWN]-(customer2:CUSTOMER),"
-				+ " (customer2:CUSTOMER)-[:OWN]->(list3:SHOPPING_LIST)-[:CONTAIN]->(products2:PRODUCT)"
-				+ " WHERE NOT (customer1:CUSTOMER)-[:OWN]->(list1:SHOPPING_LIST)-[:CONTAIN]->(products2:PRODUCT)"
-				+ " RETURN products2.name, COUNT(DISTINCT customer2) AS frequency"
-				+ " ORDER BY frequency DESC"
-				+ " LIMIT 3");
+		// Map<String, Object> parameters = new HashMap<>();
+		// parameters.put("customerId", customerId);
+		Map<String, Object> parameters = ImmutableMap.<String, Object>of("customerId", customerId);
+
+		String query = "MATCH (customer1:CUSTOMER {customerId: { customerId }})-[:OWN]->(list1:SHOPPING_LIST)-[:CONTAIN]->(products:PRODUCT)\n"
+				+ " <-[:CONTAIN]-(list2:SHOPPING_LIST)<-[:OWN]-(customer2:CUSTOMER),\n"
+				+ " (customer2:CUSTOMER)-[:OWN]->(list3:SHOPPING_LIST)-[:CONTAIN]->(products2:PRODUCT)\n"
+				+ " WHERE NOT (customer1:CUSTOMER)-[:OWN]->(list1:SHOPPING_LIST)-[:CONTAIN]->(products2:PRODUCT)\n"
+				+ " RETURN products2.name, COUNT(DISTINCT customer2) AS frequency" + " ORDER BY frequency DESC\n"
+				+ " LIMIT 3";
+		System.out.println(query);
+		final Result result = graph.getGraphDb().execute(query, parameters);
+		System.out.println();
 		resultBuilder.append("[" + customerId + "] : {");
 		int count = 0;
-		while(result.hasNext()) {
-			Map<String, Object> resultMap = result.next();
-			if(count++ % 2 == 0) {
-				resultBuilder.append("{" + resultMap.get("frequency") + " : ");
-			} else {
-				 resultBuilder.append(resultMap.get("products2.name }"));
-			}
-	
-			if (result.hasNext()) {
-				resultBuilder.append(", ");
-			} else {
-				resultBuilder.append("}\n");
-			}
+		
+		final Iterable<Map<String, Object>> resultIterable = () -> result;
+		
+		for (Map<String, Object> resultElement : resultIterable) {
 			
+		}
+		
+		while (result.hasNext()) {
+			// Map<String, Object> resultMap = result.next();
+			// if(count++ % 2 == 0) {
+			// resultBuilder.append("{" + resultMap.get("frequency") + " : ");
+			// } else {
+			// resultBuilder.append(resultMap.get("products2.name }"));
+			// }
+			//
+			// if (result.hasNext()) {
+			// resultBuilder.append(", ");
+			// } else {
+			// resultBuilder.append("}\n");
+			// }
+			resultBuilder.append(count++ + "\n");
+			System.out.println(count++ + " " + result.next());
+
 		}
 		return resultBuilder.toString();
 	}
-	
+
 	public void processQuery() {
 		String resultRow;
 		for (String customerId : graph.getDataReader().getDistinctCustomers()) {
@@ -68,5 +85,5 @@ public class RecommendationEngine {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
